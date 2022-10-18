a074ee238e197f7b766dae085e054a72e443a0c4
Tim Kopplin <timkopplin@hotmail.com>
2022-10-18 23:17:40 +0000

    assertArraysEqual amendment

--- assertArraysEqual.js
+++ assertArraysEqual.js
@@ -7,7 +7,7 @@ const assertArraysEqual = function(actual, expected) {
 };
 
 const eqArrays = function(arr1, arr2) {
-  if (isEqual(arr1, arr2) === true && isSameType(arr1, arr2) === true){
+  if (isEqual(arr1, arr2) === true && isSameType(arr1, arr2) === true) {
     return true;
   } else {
     return false;
@@ -15,9 +15,9 @@ const eqArrays = function(arr1, arr2) {
 };
 
 const isSameType = function(one, two) {
-  for (let arrOne of one){
-    for (let arrTwo of two){
-      if (typeof arrOne !== typeof arrTwo){
+  for (let arrOne of one) {
+    for (let arrTwo of two) {
+      if (typeof arrOne !== typeof arrTwo) {
         return false;
       }
     }
@@ -25,12 +25,12 @@ const isSameType = function(one, two) {
   return true;
 };
 
-const isEqual = function(one, two){
+const isEqual = function(one, two) {
   for (let uno of one) {
     for (let dos of two) {
       if (uno === dos) {
         return true;
-      } else if(uno !== dos){
+      } else if (uno !== dos) {
         return false;
       }
     }
