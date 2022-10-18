8c1999c1a72c4b5abdfa6ce36e839c6c25a534da
Tim Kopplin <timkopplin@hotmail.com>
2022-10-18 22:20:54 +0000

    eqArrays fix

--- eqArrays.js
+++ eqArrays.js
@@ -38,6 +38,12 @@ const isEqual = function(one, two){
   
 };
 
+console.log(eqArrays([1, 2, 3], [1, 2, 3])); // => true
+console.log(eqArrays([1, 2, 3], [3, 2, 1])); // => false
+
+console.log(eqArrays(["1", "2", "3"], ["1", "2", "3"])); // => true
+console.log(eqArrays(["1", "2", "3"], ["1", "2", 3])); // => false
+
 assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true); // => true
 assertEqual(eqArrays([1, 2, 3], [3, 2, 1]), false); // => false
 
