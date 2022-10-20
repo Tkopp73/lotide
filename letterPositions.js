400b9661340ea202da21bba5371dc0726855592f
Tim Kopplin <timkopplin@hotmail.com>
2022-10-20 01:27:49 +0000

    first commit: letter positions

---
+++ letterPositions.js
@@ -0,0 +1,19 @@
+const letterPositions = function(words) {
+  let letters = {};
+  for (let x = 0; x < words.length; x++) {
+    if (words[x] !== ' ') {
+      if (!letters[words[x]]) {
+        letters[words[x]] = [x];
+      } else {
+        letters[words[x]].push(x);
+      }
+    }
+  }
+
+  return letters;
+  
+};
+
+
+console.log(letterPositions("hello"));
+console.log(letterPositions("supercalifragilisticexpialidocious"));
