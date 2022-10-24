cf9d2ccd050070b4059a5427dc95764935847ab9
Tim Kopplin <timkopplin@hotmail.com>
2022-10-24 19:50:42 +0000

    Refactoring

--- assertArraysEqual.js
+++ assertArraysEqual.js
@@ -1,3 +1,4 @@
+
 const assertArraysEqual = function(actual, expected) {
   if (actual === expected) {
     console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`);
@@ -6,40 +7,6 @@ const assertArraysEqual = function(actual, expected) {
   }
 };
 
-const eqArrays = function(arr1, arr2) {
-  if (isEqual(arr1, arr2) === true && isSameType(arr1, arr2) === true) {
-    return true;
-  } else {
-    return false;
-  }
-};
 
-const isSameType = function(one, two) {
-  for (let arrOne of one) {
-    for (let arrTwo of two) {
-      if (typeof arrOne !== typeof arrTwo) {
-        return false;
-      }
-    }
-  }
-  return true;
-};
 
-const isEqual = function(one, two) {
-  for (let uno of one) {
-    for (let dos of two) {
-      if (uno === dos) {
-        return true;
-      } else if (uno !== dos) {
-        return false;
-      }
-    }
-  }
-  
-};
-
-assertArraysEqual(eqArrays([1, 2, 3], [1, 2, 3]), true); // => true
-assertArraysEqual(eqArrays([1, 2, 3], [3, 2, 1]), false); // => false
-
-assertArraysEqual(eqArrays(["1", "2", "3"], ["1", "2", "3"]), true); // => true
-assertArraysEqual(eqArrays(["1", "2", "3"], ["1", "2", 3]), false); // => false
+module.exports = assertArraysEqual;
--- test/eqArraysTest.js
+++ test/eqArraysTest.js
@@ -1,10 +1,9 @@
 const eqArrays = require('../eqArrays');
-const assertEqual = require('../assertEqual');
 
 
 
-assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true); // => true
-assertEqual(eqArrays([1, 2, 3], [3, 2, 1]), false); // => false
+console.log(eqArrays([1, 2, 3], [1, 2, 3]));
+console.log(eqArrays([1, 2, 3], [3, 2, 1]));
 
-assertEqual(eqArrays(["1", "2", "3"], ["1", "2", "3"]), true); // => true
-assertEqual(eqArrays(["1", "2", "3"], ["1", "2", 3]), false); // => false
+console.log(eqArrays(["1", "2", "3"], ["1", "2", "3"]));
+console.log(eqArrays(["1", "2", "3"], ["1", "2", 3]));
